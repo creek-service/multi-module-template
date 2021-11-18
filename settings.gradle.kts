a60ad63a1dffe4b3b0589b9dfa90a2fd3946526b
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2021-11-18 14:19:31 +0000

    Rename to multi (#4)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "template" // ChangeMe: set the root project name with the module name.
+rootProject.name = "multi-module-template" // ChangeMe: set the root project name with the module name.
 
 include(
     "example"
